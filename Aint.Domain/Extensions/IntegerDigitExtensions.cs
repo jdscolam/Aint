7b48604f706d96910d2881d6fc0ccde1526113ea
jdscolam <jdscolam@fastmail.fm>
2016-11-26 14:47:09 -0600

    Added recursive integer summing

---
+++ Aint.Domain/Extensions/IntegerDigitExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Aint.Domain.Extensions
+{
+    public static class IntegerDigitExtensions
+    {
+        public static int SumDigitsRecursively(this int intToSum)
+        {
+            intToSum = Math.Abs(intToSum);
+
+            if (intToSum < 10)
+                return intToSum;
+
+            var sum = intToSum % 10;
+
+            sum += SumDigitsRecursively(intToSum / 10);
+
+            return sum;
+        }
+    }
+}
---
+++ Aint.Tests.Unit/SumDigitsTests.cs
@@ -0,0 +1,58 @@
+﻿using Aint.Domain.Extensions;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Aint.Tests.Unit
+{
+    [TestFixture]
+    public class SumDigitsTests
+    {
+        [Test]
+        public void SumDigitsRecursively_can_return_the_sum_of_integers()
+        {
+            //Setup.
+            const int intToSum = 12345;
+            const int expectedSum = 15;
+
+            //Execute.
+            var sum = intToSum.SumDigitsRecursively();
+
+            //Verify.
+            sum.ShouldBeEquivalentTo(expectedSum);
+
+            //Teardown.
+        }
+
+        [Test]
+        public void SumDigitsRecursively_can_return_the_sum_of_negative_integers()
+        {
+            //Setup.
+            const int intToSum = -12345;
+            const int expectedSum = 15;
+
+            //Execute.
+            var sum = intToSum.SumDigitsRecursively();
+
+            //Verify.
+            sum.ShouldBeEquivalentTo(expectedSum);
+
+            //Teardown.
+        }
+
+        [Test]
+        public void SumDigitsRecursively_can_return_the_sum_of_single_integers()
+        {
+            //Setup.
+            const int intToSum = 8;
+            const int expectedSum = 8;
+
+            //Execute.
+            var sum = intToSum.SumDigitsRecursively();
+
+            //Verify.
+            sum.ShouldBeEquivalentTo(expectedSum);
+
+            //Teardown.
+        }
+    }
+}
