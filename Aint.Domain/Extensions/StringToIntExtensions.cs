a2f2e200f396bbbf0290690fa455dac5abe0e4c7
jdscolam <jdscolam@fastmail.fm>
2016-11-27 15:13:28 -0600

    Added a recursive string to int converter
    Tested out my new mechanical keyboard with this practise.  It's been
    very neat so far, but still getting used to it.

---
+++ Aint.Domain/Extensions/StringToIntExtensions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace Aint.Domain.Extensions
+{
+    public static class StringToIntExtensions
+    {
+        public static int ConvertToIntRecursively(this string stringToConvert)
+        {
+            var sign = 1;
+
+            //Prep string by removing non-numeric or sign characters.
+            stringToConvert = Regex.Replace(stringToConvert, "[^0-9.-]", "");
+
+            if(stringToConvert[0] == '-')
+            {
+                stringToConvert = stringToConvert.Remove(0, 1);
+                sign = -1;
+            }
+
+            if (stringToConvert[0] == '.')
+                return 0;
+
+            //The result will come out negative if we've hit a decimal, so we'll get the absolute value then apply the appropriate sign.
+            var value = sign * Math.Abs(ConvertToIntInternal(stringToConvert));
+
+            return value;
+        }
+
+        private static int ConvertToIntInternal(this string stringToConvert)
+        {
+            //break if we're at the end.
+            if (stringToConvert.Length == 1)
+                return CharUnicodeInfo.GetDecimalDigitValue(stringToConvert[0]);
+
+            var currentDigitChar = stringToConvert[stringToConvert.Length - 1];
+
+            var currentDigit = CharUnicodeInfo.GetDecimalDigitValue(currentDigitChar);
+
+            if (currentDigit == -1)
+                return -1 * ConvertToIntInternal(stringToConvert.Remove(stringToConvert.Length - 1));
+
+            var resultToLeftOfCurrentDigit = ConvertToIntInternal(stringToConvert.Remove(stringToConvert.Length - 1));
+
+            //We hit a decimal.
+            if (resultToLeftOfCurrentDigit < 0)
+                return resultToLeftOfCurrentDigit;
+
+            var total = currentDigit + 10 * ConvertToIntInternal(stringToConvert.Remove(stringToConvert.Length - 1));
+            return total;
+        }
+    }
+}
---
+++ Aint.Tests.Unit/StringToIntTests.cs
@@ -0,0 +1,76 @@
+﻿using Aint.Domain.Extensions;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Aint.Tests.Unit
+{
+    [TestFixture]
+    public class StringToIntTests
+    {
+        [Test]
+        public void ConvertToIntRecursively_can_convert_a_string_to_an_integer()
+        {
+            //Setup.
+            const string stringToConvert = "12345";
+
+            //Execute.
+            var converted = stringToConvert.ConvertToIntRecursively();
+
+            //Verify.
+            converted.ShouldBeEquivalentTo(12345);
+
+            //Teardown.
+        }
+
+        [Test]
+        public void ConvertToIntRecursively_can_handle_non_numeric_characters()
+        {
+            //Setup.
+            const string stringToConvert = "12,345";
+            const string stringToConvert2 = "123 45";
+
+            //Execute.
+            var converted = stringToConvert.ConvertToIntRecursively();
+            var converted2 = stringToConvert2.ConvertToIntRecursively();
+
+            //Verify.
+            converted.ShouldBeEquivalentTo(12345);
+            converted2.ShouldBeEquivalentTo(12345);
+
+            //Teardown.
+        }
+
+        [Test]
+        public void ConvertToIntRecursively_can_handle_negative_values()
+        {
+            //Setup.
+            const string stringToConvert = "-12345";
+
+            //Execute.
+            var converted = stringToConvert.ConvertToIntRecursively();
+
+            //Verify.
+            converted.ShouldBeEquivalentTo(-12345);
+
+            //Teardown.
+        }
+
+        [Test]
+        public void ConvertToIntRecursively_can_handle_decimals()
+        {
+            //Setup.
+            const string stringToConvert = "123.45";
+            const string stringToConvert2 = ".12345";
+
+            //Execute.
+            var converted = stringToConvert.ConvertToIntRecursively();
+            var converted2 = stringToConvert2.ConvertToIntRecursively();
+
+            //Verify.
+            converted.ShouldBeEquivalentTo(123);
+            converted2.ShouldBeEquivalentTo(0);
+
+            //Teardown.
+        }
+    }
+}
